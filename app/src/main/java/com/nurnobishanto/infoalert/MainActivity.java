bc827133e58756f928dc9c34295bef67ed374f16
Md Nurnobi Hosen <mdnurnobihosen1@gmail.com>
2022-04-25 10:27:46 +0600

    Main Activity Nav Menu Added

--- app/src/main/java/com/nurnobishanto/infoalert/MainActivity.java
+++ app/src/main/java/com/nurnobishanto/infoalert/MainActivity.java
@@ -1,14 +1,80 @@
 package com.nurnobishanto.infoalert;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+import com.nurnobishanto.infoalert.Fragment.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setTitle("Home");
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,
+                new HomeFragment()).commit();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        drawer.closeDrawer(GravityCompat.START);
+                        getSupportActionBar().setTitle("Home");
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,
+                                new HomeFragment()).commit();
+                        break;
+
+                }
+                return true;
+            }
+        });
+    }
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        getSupportActionBar().setTitle("Home");
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,
+                new HomeFragment()).commit();
+        if (doubleBackToExitPressedOnce) {
+
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
