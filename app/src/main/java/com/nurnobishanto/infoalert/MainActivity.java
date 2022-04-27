d2fe7cf84ff4ab09ba3d0a4f0aab79c1d40e83bc
Md Nurnobi Hosen <mdnurnobihosen1@gmail.com>
2022-04-27 19:17:40 +0600

    Button name Fix

--- app/src/main/java/com/nurnobishanto/infoalert/MainActivity.java
+++ app/src/main/java/com/nurnobishanto/infoalert/MainActivity.java
@@ -30,9 +30,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
